5d932ded88e7a36a918e4644f8646e36846a8eb0
YogeshTembe <yogeshtembe80@gmail.com>
2021-04-08 20:28:40 +0530

    helpme

--- djangocrud/PySpice2/Spice/NgSpice/Shared.py
+++ djangocrud/PySpice2/Spice/NgSpice/Shared.py
@@ -415,7 +415,8 @@ class NgSpiceShared:
                 path = 'libngspice{}.dylib'
 
             elif ConfigInstall.OS.on_linux:
-                path = 'libngspice{}.so'
+                path =os.path.abspath('.//PySpice2//Spice//NgSpice//Spice64_dll//dll-vs//ngspice.dll')
+                #path = 'libngspice{}.so'
 
             else:
                 raise NotImplementedError
